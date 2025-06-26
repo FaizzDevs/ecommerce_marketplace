06d2db00a0ea27f0be0e6acfed38f2b6f7867f73
FaizzDevs <faizdevs@gmail.com>
2025-06-26 21:35:59 +0700

    update code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Progress } from "@/components/ui/progress";
+import { Textarea } from "@/components/ui/textarea";
+
 export default function Home() {
     return (
-        <div>
-            <p className="text-rose-500">Hellow World</p>
+        <div className="flex flex-col gap-y-4 p-6">
+            <div>
+                <Button variant="elevated">
+                    I am button
+                </Button>
+            </div>
+            <div>
+                <Input className="" />
+            </div>
+            <div>
+              <Progress value={70} />
+            </div>
+            <div>
+              <Textarea />
+            </div>
         </div>
     )
 }
